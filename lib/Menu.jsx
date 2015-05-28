01c25a9616b073c33fa0628257475cf14f1528e3
Joe Nelson <cred+github@begriffs.com>
2015-05-28 14:12:43 -0700

    Next-level menu shit

--- lib/Menu.jsx
+++ lib/Menu.jsx
@@ -3,40 +3,55 @@
 var React = require('react');
 var cx = require('classnames');
 
-function isInt(i) {
-  // http://stackoverflow.com/a/14794066/3102996
-  return !isNaN(i) && parseInt(Number(i)) == i && !isNaN(parseInt(i, 10))
-}
+var propTypeNonnegInteger = function(props, name, componentName) {
+  function isInt(i) {
+    // http://stackoverflow.com/a/14794066/3102996
+    return !isNaN(i) && parseInt(Number(i)) == i && !isNaN(parseInt(i, 10))
+  }
+
+  if(!isInt(props[name]) || props[name] < 0) {
+    return new Error(
+      "Invalid prop `" + name +"` with value `" + props[name] +
+      "` supplied to `" + componentName +
+      "`, expected nonnegative integer");
+  }
+};
 
 var Item = React.createClass({
   propTypes: {
     text: React.PropTypes.string,
     url: React.PropTypes.string,
-    description: React.PropTypes.string
+    description: React.PropTypes.string,
+    level: propTypeNonnegInteger
+  },
+
+  getDefaultProps: function () {
+    return { level: 0 };
   },
 
   render: function() {
-    var inner = this.props.url
+    var inner = (this.props.url
       ? <a className="text" href={this.props.url}>{this.props.text}</a>
-      : <span className="text">{this.props.text}</span>
+      : <span className="text">{this.props.text}</span>);
 
-    return (this.props.description
+    var described = this.props.description
       ? <dl><dt>{inner}</dt><dd>{this.props.description}</dd></dl>
-      : inner
+      : inner;
+
+    return (this.props.items
+      ? <details>
+          <summary>{described}</summary>
+          <Menu className="options" level={this.props.level+1}
+            items={this.props.items} />
+        </details>
+      : described
     );
   }
 });
 
 var Menu = React.createClass({
   propTypes: {
-    level: function(props, name, componentName) {
-      if(!isInt(props[name]) || props[name] < 0) {
-        return new Error(
-          "Invalid prop `" + name +"` with value `" + props[name] +
-          "` supplied to `" + componentName +
-          "`, expected nonnegative integer");
-      }
-    },
+    level: propTypeNonnegInteger,
     items: React.PropTypes.array
   },
 
@@ -48,12 +63,17 @@ var Menu = React.createClass({
   },
 
   render: function() {
-    var lvl = "level-" + this.props.level;
+    var level = this.props.level;
+    var levelClass = "level-" + this.props.level;
+    var classes = cx(this.props.className, 'shwp-menu', levelClass);
+
     var items = this.props.items.map(function (i) {
-      return <li className={lvl}><Item {...i} /></li>
+      return <li className={levelClass}>
+          <Item level={level} {...i} />
+        </li>;
     });
     return (
-      <ul className={lvl}>
+      <ul className={classes}>
         {items}
       </ul>
     );
--- preview.jsx
+++ preview.jsx
@@ -5,7 +5,11 @@ var Menu = require('./lib/Menu.jsx');
 var items = [
   { url: "http://google.com", text: "Search Engine" },
   { url: "http://wagonhq.com", text: "Wag", description: "Short for wagon" },
-  { text: "Just Fun", description: "See, no href" }
+  { text: "Just Fun", description: "See, no href",
+    items: [
+      { url: "http://google2.com", text: "Another Search Engine" }
+    ]
+  }
 ];
 
 React.render(<Menu items={items} />, document.getElementById('content'));
